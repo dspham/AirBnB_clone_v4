38c722fcca2b428cf4a3fef931ddabaa54142c63
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-24 16:10:38 -0800

    amenities list

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -17,72 +17,17 @@ $(document).ready(function () {
         }
     });
 
-    $.ajax({
-        url: 'http://0.0.0.0:5001/api/v1/places_search/',
-        method: 'POST',
-        dataType: 'json',
-        contentType: 'application/JSON',
-        data: JSON.stringify({}),
-        success: function (data) {
-            $.each(data, function (k, v) {
-                console.log(k, v);
-                $(`<article class='places_articles'>
-          <div class="title">
-          <h2>${v.name}</h2>
-          <div class="price_by_night">
-          ${v.price_by_night}
-          </div>
-          </div>
-          <div class="information">
-          <div class="max_guest">
-          <i class = "fa fa-users fa-3x" aria - hidden = "true"></i>
-          <br />
-          ${v.max_guest} Guests
-          </div>
-          <div class="number_rooms">
-          <i class="fa fa-bed fa-3x" aria - hidden = "true" > </i>
-          <br />
-          ${v.number_rooms} Bedrooms
-          </div>
-          <div class="number_bathrooms">
-          <i class="fa fa-bath fa-3x" aria - hidden = "true"></i>
-          <br />
-          ${v.number_bathrooms} Bathroom
-          </div>
-          </div>
-          <div class="user">
-          <strong> Owner: ${v.user.first_name} ${v.user.last_name}</strong>
-          </div>
-          <div class="description">
-          ${v.description}
-          </div>
-          </article>`).appendTo('.places');
-            });
-        }
-    });
-
-    $('#search_btn').click((el) => {
-      let res = [];
-      $('.amenities li input').each((idx, elem) => { 
-        if (elem.checked) {
-          console.log(elem.dataset);
-          res.push(elem.dataset.id)
-        }
-      });
-      if (res.length) {
-       $.ajax({
+    function placeSearch(postdata){
+      $('.places_articles').remove();
+      $.ajax({
           url: 'http://0.0.0.0:5001/api/v1/places_search/',
           method: 'POST',
           dataType: 'json',
           contentType: 'application/JSON',
-          data: JSON.stringify({
-            amenities: res
-          }),
+          data: JSON.stringify(postdata),
           success: function (data) {
-            $('.places_articles').remove();
               $.each(data, function (k, v) {
-                  console.log(k, v);
-                $(`<article class='places_articles'>
+                  $(`<article class='places_articles'>
                     <div class="title">
                     <h2>${v.name}</h2>
                     <div class="price_by_night">
@@ -115,7 +60,20 @@ $(document).ready(function () {
                     </article>`).appendTo('.places');
               });
           }
-    });
+        });
+    }
+
+    placeSearch({});
+
+    $('#search_btn').click((el) => {
+      let res = [];
+      $('.amenities li input').each((idx, elem) => { 
+        if (elem.checked) {
+          res.push(elem.dataset.id);
+        }
+      });
+      if (res.length) {
+        placeSearch({amenities:res});
       }
     });
 });
